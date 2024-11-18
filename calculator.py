1e4255cc609a6389514e5f77fe850763f4ecaa22
HypnosDevs <130528462+HypnosDevs@users.noreply.github.com>
2024-11-18 18:51:50 +0000

    Added the test case and fixed the calculator class

--- calculator.py
+++ calculator.py
@@ -3,24 +3,91 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        negativeA = 0
+        negativeB = 0
+        if a == 0 or b==0:
+            return 0
+        if a < 0:
+            tmp = a
+            a -= a
+            a -= tmp
+            negativeA = 1
+        if b < 0:
+            tmp = b
+            b -= b
+            b -= tmp
+            negativeB = 1
+        
+        for i in range(b):
             result = self.add(result, a)
+        if (negativeA == 1 and negativeB == 0) or (negativeA == 0 and negativeB == 1):
+            tmp = result
+            result -= result
+            result -= tmp
         return result
 
     def divide(self, a, b):
+        if b == 0:
+            return "Error can't divide by zero"
+        if a == 0:
+            return 0
         result = 0
-        while a > b:
+
+        negativeFlag = False
+        if (a<0 and b>0) or (b<0 and a>0):
+            negativeFlag = True
+        
+        if a < 0:
+            tmp = a
+            a -= a
+            a -= tmp
+         
+        if b < 0:
+            tmp = b
+            b -= b
+            b -= tmp
+
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
+        
+        if negativeFlag:
+            tmp = result
+            result -= result
+            result -= tmp
         return result
     
     def modulo(self, a, b):
-        while a <= b:
-            a = a-b
+        if b == 0:
+            return "Error: can't perform modulo by zero"
+        
+        negativeFlag = False
+        
+        if (a<0 and b>0) or (a<0 and b<0):
+            negativeFlag = True
+        if a < 0:
+            tmp = a
+            a -= a
+            a -= tmp
+
+        if b < 0:
+            tmp = b
+            b -= b
+            b -= tmp
+
+
+        while a >= b:
+            a = self.subtract(a, b)
+        
+        if negativeFlag:
+            tmp = a
+            a -= a
+            a -= tmp
+
         return a
 
 # Example usage:
--- test_calculator.py
+++ test_calculator.py
@@ -8,6 +8,35 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(self.calc.add(2, -2), 0)
+        self.assertEqual(self.calc.add(-3, -5), -8)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(1, 2), -1)
+        self.assertEqual(self.calc.subtract(-2, -2), 0)
+        self.assertEqual(self.calc.subtract(-3, -5), 2)
+    
+    def test_multiple(self):
+        self.assertEqual(self.calc.multiply(2, 0), 0)
+        self.assertEqual(self.calc.multiply(2, -3), -6)
+        self.assertEqual(self.calc.multiply(-5, -3), 15)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(2, 0), "Error can't divide by zero")
+        self.assertEqual(self.calc.divide(0, -3), 0)
+        self.assertEqual(self.calc.divide(-5, -5), 1)
+        self.assertEqual(self.calc.divide(-10, 5), -2)
+        self.assertEqual(self.calc.divide(5, 2), 2)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(10, 3), 1)
+        self.assertEqual(self.calc.modulo(-10, -3), -1)
+        self.assertEqual(self.calc.modulo(25, 7), 4)
+        self.assertEqual(self.calc.modulo(0, 3), 0)
+        self.assertEqual(self.calc.modulo(3, 1), 0)
+        self.assertEqual(self.calc.modulo(1, 7), 1)
+       
+
 
     # Add the following test methods to the TestCalculator class:
 
